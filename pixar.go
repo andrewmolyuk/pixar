500355b2d6aa2cbea097977a11963392d39c0f2b
Andrew Molyuk <andrew@molyuk.com>
2022-08-14 10:41:30 +0300

    Fix linter issues

--- pixar.go
+++ pixar.go
@@ -6,13 +6,14 @@ type BuildInfo struct {
 	Commit  string
 }
 
-// Action is a type of action to be performed by a command
+// Action is a type of action applied to file
 type Action string
 
+// List of actions applied to files
 const (
-	Copy Action = "Copy" // Copy is an action to copy a file
-	Move Action = "Move" // Move is an action to move a file
-	Skip Action = "Skip" // Skip is an action that skips the file
+	Copy Action = "Copy"
+	Move Action = "Move"
+	Skip Action = "Skip"
 )
 
 // FileAction is a type of action to be performed on file
