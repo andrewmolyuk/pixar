4664c5c8d2949bd8834f2ff076c91111a8241052
Andrew Molyuk <andrew@molyuk.com>
2022-08-14 13:28:38 +0300

    Fix linter issues

--- semaphore/semaphore.go
+++ semaphore/semaphore.go
@@ -2,33 +2,40 @@ package semaphore
 
 import "sync"
 
+// Semaphore is a semaphore implementation.
 type Semaphore struct {
 	c  chan struct{}
 	wg *sync.WaitGroup
 }
 
+// NewSemaphore creates a new semaphore with the given limit.
 func NewSemaphore(maxConcurrentOps uint) *Semaphore {
 	return &Semaphore{make(chan struct{}, maxConcurrentOps), new(sync.WaitGroup)}
 }
 
+// Acquire acquires a semaphore.
 func (s *Semaphore) Acquire() {
 	s.wg.Add(1)
 	s.c <- struct{}{}
 }
 
+// Release releases a semaphore.
 func (s *Semaphore) Release() {
 	<-s.c
 	s.wg.Done()
 }
 
+// Wait waits for all acquired semaphores to be released.
 func (s *Semaphore) Wait() {
 	s.wg.Wait()
 }
 
+// Limit returns the limit of the semaphore.
 func (s *Semaphore) Limit() int {
 	return cap(s.c)
 }
 
+// Count returns the number of acquired semaphores.
 func (s *Semaphore) Count() int {
 	return len(s.c)
 }
